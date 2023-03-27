cbc74e5239363d75cce695625c1d3dd82a14ef8e
Edward Fernando <edward.fer@gmail.com>
2023-03-27 17:14:23 +0700

    starting the balancer with the new code structure

--- main.go
+++ main.go
@@ -3,43 +3,22 @@ package main
 import (
 	"log"
 	"mylb/balancer"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
 )
 
 func main() {
-
+	// move this to viper
 	originServerList := []string{
-		"localhost:8081",
-		"localhost:8082",
-		"localhost:8083",
-		"localhost:8084",
+		"http://localhost:8081",
+		"http://localhost:8082",
+		"http://localhost:8083",
+		"http://localhost:8084",
 	}
 
-	var backends []*balancer.Backend
-	for _, urlString := range originServerList {
-
-		url := &url.URL{
-			Scheme: "http",
-			Host:   urlString,
-		}
-
-		backend := &balancer.Backend{
-			URL:          url,
-			Alive:        true, // todo: add checking for this
-			ReverseProxy: httputil.NewSingleHostReverseProxy(url),
-		}
-
-		backends = append(backends, backend)
+	pool, err := balancer.NewLoadBalancer(originServerList)
+	if err != nil {
+		panic(err)
 	}
 
-	serverPool := &balancer.ServerPool{Backends: backends}
-
-	server := &http.Server{
-		Addr:    ":8000",
-		Handler: serverPool,
-	}
-
-	log.Fatal(server.ListenAndServe())
+	log.Default().Println("Starting server on port 8000....")
+	log.Fatal(pool.ListenAndServe())
 }
