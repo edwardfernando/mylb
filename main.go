edc71b8aa5a5127d25e1b77235aef508dcd96a4e
Edward Fernando <edward.fer@gmail.com>
2023-03-27 16:33:46 +0700

    use the new balancer struct to serve request with loadbalancer

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"log"
+	"mylb/balancer"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+
+	originServerList := []string{
+		"localhost:8081",
+		"localhost:8082",
+		"localhost:8083",
+		"localhost:8084",
+	}
+
+	var backends []*balancer.Backend
+	for _, urlString := range originServerList {
+
+		url := &url.URL{
+			Scheme: "http",
+			Host:   urlString,
+		}
+
+		backend := &balancer.Backend{
+			URL:          url,
+			Alive:        true, // todo: add checking for this
+			ReverseProxy: httputil.NewSingleHostReverseProxy(url),
+		}
+
+		backends = append(backends, backend)
+	}
+
+	serverPool := &balancer.ServerPool{Backends: backends}
+
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: serverPool,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
